c6bc5caf7deeeaba105c0d4a2afb4a45e7cd24ec
James Lang <langjam350@gmail.com>
2025-06-28 17:01:37 -0700

    Ensured proper hosting artifact generation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Disable static export temporarily to test build
-    // output: 'export',
+    output: 'export',
+    trailingSlash: true,
     images: {
         unoptimized: true,
     },
@@ -11,10 +11,6 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    // Remove experimental features that might cause issues
-    // experimental: {
-    //     esmExternals: false,
-    // },
 }
 
 module.exports = nextConfig
