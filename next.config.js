76e35d00bade15586d2d44f7619195c86288358f
James Lang <langjam350@gmail.com>
2025-06-30 18:53:03 -0700

    Adding more context to next.config.js and ts.config.sjon

--- next.config.js
+++ next.config.js
@@ -11,6 +11,16 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    serverExternalPackages: ['firebase', 'firebase-admin'],
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+            net: false,
+            tls: false,
+        }
+        return config
+    }
 }
 
 module.exports = nextConfig
